8e514b2b0563cbd1f2a115fbc6a314daed5978e0
Aarushi <aarushijha2505@gmail.com>
2026-04-10 20:22:17 +0530

    minor change

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Synapse/',   
+  base: process.env.NODE_ENV === 'production' ? '/Synapse/' : '/',
   esbuild: {
     loader: 'jsx'
   },
